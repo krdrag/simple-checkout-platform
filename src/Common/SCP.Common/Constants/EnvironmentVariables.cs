1d9d2aba2ee4b59a891a14c4283bab67c4c836f9
krdrag <krzysztof.dra@gmail.com>
2022-09-06 11:09:12 +0200

    Update EnvironmentVariables.cs

--- src/Common/SCP.Common/Constants/EnvironmentVariables.cs
+++ src/Common/SCP.Common/Constants/EnvironmentVariables.cs
@@ -3,18 +3,19 @@
     public static class EnvironmentVariables
     {
         // Redis
-        public static string RedisHost => Environment.GetEnvironmentVariable("REDIS_HOST") ?? string.Empty;
-        public static string RedisPwd => Environment.GetEnvironmentVariable("REDIS_PWD") ?? string.Empty;
+        public static string RedisHost => Environment.GetEnvironmentVariable("REDIS_HOST") ?? throw new KeyNotFoundException("Redis host environment variable not found!");
+        public static string RedisPwd => Environment.GetEnvironmentVariable("REDIS_PWD") ?? throw new KeyNotFoundException("Redis password environment variable not found!");
 
         // RabbitMQ
-        public static string RabbitHostVar => Environment.GetEnvironmentVariable("RabbitMQ_Host") ?? string.Empty;
-        public static string RabbitUserEnvVar => Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_USER") ?? string.Empty;
-        public static string RabbitPassEnvVar => Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_PASS") ?? string.Empty;
+        public static string RabbitHostVar => Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? throw new KeyNotFoundException("Rabbit host environment variable not found!");
+        public static string RabbitUserEnvVar => Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_USER") ?? throw new KeyNotFoundException("Rabbit user environment variable not found!");
+        public static string RabbitPassEnvVar => Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_PASS") ?? throw new KeyNotFoundException("Rabbit password environment variable not found!");
 
         // Identity
-        public static string POSClientPasswordEnvVar => Environment.GetEnvironmentVariable("POS_CLIENT_PASSWORD") ?? string.Empty;
-        public static string PostmanClientPasswordEnvVar => Environment.GetEnvironmentVariable("POSTMAN_CLIENT_PASSWORD") ?? string.Empty;
-        public static string TransactionResourceEnvVar => Environment.GetEnvironmentVariable("TRANSACTION_RESOURCE_PASSWORD") ?? string.Empty;
-        public static string SessionResourceEnvVar => Environment.GetEnvironmentVariable("SESSION_RESOURCE_PASSWORD") ?? string.Empty;
+        public static string IdentityAuthority => Environment.GetEnvironmentVariable("IDENTITY_AUTHORITY") ?? throw new KeyNotFoundException("Authority variable not found!");
+        public static string POSClientPasswordEnvVar => Environment.GetEnvironmentVariable("POS_CLIENT_PASSWORD") ?? throw new KeyNotFoundException("Pos client password environment variable not found!");
+        public static string PostmanClientPasswordEnvVar => Environment.GetEnvironmentVariable("POSTMAN_CLIENT_PASSWORD") ?? throw new KeyNotFoundException("Postman client password host environment variable not found!");
+        public static string TransactionResourceEnvVar => Environment.GetEnvironmentVariable("TRANSACTION_RESOURCE_PASSWORD") ?? throw new KeyNotFoundException("Transaction service client password host environment variable not found!");
+        public static string SessionResourceEnvVar => Environment.GetEnvironmentVariable("SESSION_RESOURCE_PASSWORD") ?? throw new KeyNotFoundException("Session service client password environment variable not found!");
     }
 }
